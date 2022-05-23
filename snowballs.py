92ae8f61e83a3cde2d63cab1fa20c2ee78856a21
cher0ku93 <104194626+cher0ku93@users.noreply.github.com>
2022-05-23 18:40:33 +0200

    Add files via upload

--- snowballs.py
+++ snowballs.py
@@ -16,28 +16,28 @@ class Snowballs(pygame.sprite.Sprite):
         self.image = pygame.image.load('images/snowball.png')
         self.image = pygame.transform.scale ( self.image , (160 , 160) )
         self.rect = self.image.get_rect()
-        self.rect.x = 300 + random.randint(400, 800) # Position du traineau sur l'axe des abscisses
-        self.rect.y = -10  # Position du traineau sur l'axe des ordonnées
-        self.velocityX = 100
+        self.rect.x = 300 + random.randint(400, 800) # Position de la boule sur l'axe des abscisses
+        self.rect.y = -10  # Position de la boule sur l'axe des ordonnées
+        self.velocityX = 20
         self.velocityY = -10
 
     def scrolling(self):
-        # S'il y a une collision entre une guirlande et le joueur :
+        # S'il y a une collision entre une boule et le joueur :
         if self.game.check_collision(self, self.game.all_players):
             self.game.player.damage()
-            self.remove() # Suppression de la guirlande touchée
-            self.game.spawn_snowballs() # Apparition d'une nouvelle guirlande
+            self.remove() # Suppression de la boule touchée
+            self.game.spawn_snowballs() # Apparition d'une nouvelle boule
 
         # Sinon
         else:
-            self.rect.x -= self.velocityY # Les guirlandes défilent
+            self.rect.x -= self.velocityY # Les boules défilent
             self.rect.y = self.velocityX
 
 
         if self.rect.x <= -160:
-            self.rect.x = 600 + random.randint(300, 1000)  # Apparition d'une nouvelle guirlande
+            self.rect.x = 600 + random.randint(300, 1000)  # Apparition d'une nouvelle boules
 
-    # Fonction qui permet de supprimer les guirlandes
+    # Fonction qui permet de supprimer les boules
     def remove(self):
         self.game.all_tinsels.remove(self)
 
